28a7642e445e21bc5efae56a0f47110fde717415
SamuelFilgueira <leumas685@gmail.com>
2025-05-22 17:02:45 -0300

    Criando o banco e aplicando a migrations

---
+++ DevJobs.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using DevJobs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevJobs.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<Candidatura> Candidaturas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+
+        mb.Entity<Job>()
+            .HasOne(u => u.Recrutador)
+            .WithMany(j => j.VagasPublicadas)
+            .HasForeignKey(j => j.RecrutadorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        mb.Entity<Candidatura>()
+           .HasOne(c => c.User)
+           .WithMany(u => u.Candidaturas)
+           .HasForeignKey(c => c.UserId)
+           .OnDelete(DeleteBehavior.Restrict);
+
+        mb.Entity<Candidatura>()
+            .HasOne(j => j.Job)
+            .WithMany(j => j.Candidaturas)
+            .HasForeignKey(j => j.JobId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        mb.Entity<Job>()
+            .Property(j => j.Skills)
+            .HasConversion(v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
+            v => JsonSerializer.Deserialize<List<string>>(v, new JsonSerializerOptions())!
+            );
+
+        base.OnModelCreating(mb);
+    }
+}
---
+++ DevJobs.Infrastructure/Migrations/20250522191924_InitialMigration.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevJobs.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PretensaoSalarial = table.Column<double>(type: "float", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroCelular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    Habilidades = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senioridade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salario = table.Column<double>(type: "float", nullable: false),
+                    Localidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecrutadorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Skills = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jobs_Users_RecrutadorId",
+                        column: x => x.RecrutadorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidaturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataCandidatura = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Candidaturas_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Jobs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Candidaturas_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidaturas_JobId",
+                table: "Candidaturas",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidaturas_UserId",
+                table: "Candidaturas",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_RecrutadorId",
+                table: "Jobs",
+                column: "RecrutadorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidaturas");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
