8cd9ec878e157bcd5a77e6a4793e72860bf0d520
SamuelFilgueira <leumas685@gmail.com>
2025-05-22 17:49:15 -0300

    Criando os DTOs

---
+++ DevJobs.Application/DTOs/User/LoginUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DevJobs.Application.DTOs.User;
+
+public class LoginUserDto
+{
+    string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ DevJobs.Application/DTOs/User/RegisterUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DevJobs.Application.DTOs.User;
+
+public class RegisterUserDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public double PretensaoSalarial { get; set; }
+    public string Descricao { get; set; }
+    public string NumeroCelular { get; set; }
+}
